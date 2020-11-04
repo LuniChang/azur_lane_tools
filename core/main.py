bd45f0e3d9863f0b271617dd2ce9fd643cb7415d
ling <244410210@qq.com>
2020-11-04 13:02:02 +0800

    fix bug

--- core/main.py
+++ core/main.py
@@ -81,9 +81,9 @@ tk.Button(fm1, text="结束8-1", width=20, height=1,
 
 def initAct():
     team1BattleMaxCount=tk.IntVar()
-    team1BattleMaxCount.set(5)
+    team1BattleMaxCount.set(4)
     team2BattleMaxCount=tk.IntVar()
-    team2BattleMaxCount.set(1)
+    team2BattleMaxCount.set(2)
 
 
    
@@ -98,7 +98,7 @@ def initAct():
 
 
     model = tk.IntVar()
-    model.set(1)
+    model.set(0)
     def toChangeModel():
         replyMapActivity.setFindEnemysMode(model.get())  
 
