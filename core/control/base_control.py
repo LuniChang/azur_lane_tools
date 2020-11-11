83645f3b036ed198f4ccc6537214f0015295abd9
LuniChang <244410210@qq.com>
2020-11-11 21:15:41 +0800

    fix bug

--- core/control/base_control.py
+++ core/control/base_control.py
@@ -303,7 +303,7 @@ class BaseControl:
 
         nowHash = screen.winScreenHash(self.handle)
         res = screen.alikeHash(self._currentWinHash,
-                               nowHash, 0.5)
+                               nowHash, 0.6)
         self._currentWinHash = nowHash
         print("isSameWin",self._currentWinHash,nowHash)
         return res
